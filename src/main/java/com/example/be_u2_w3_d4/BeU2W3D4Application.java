ba7fd60c91c0f275501ee674de732bc689a99ca5
Massimo Pizzicannella <maxpizzi2605@gmail.com>
2025-02-20 12:42:57 +0100

    Initial commit

---
+++ src/main/java/com/example/be_u2_w3_d4/BeU2W3D4Application.java
@@ -0,0 +1,13 @@
+package com.example.be_u2_w3_d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W3D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W3D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/be_u2_w3_d4/BeU2W3D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_u2_w3_d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W3D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
